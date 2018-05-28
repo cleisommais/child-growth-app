876ebe803341b45d0b081f2c5226957359cf6674
Cleison Melo <cleisommais@gmail.com>
2018-05-28 22:04:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Building a Mobile App with Ionic 3
+
+## child-growth
+
+App to help parents and patients with growth related disorders.
+
+## Instructions
+
+Start the server
+
+  `ionic serve`
+
+
+
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-login',
+  templateUrl: 'login.html'
 })
-export class ContactPage {
+export class LoginPage {
 
   constructor(public navCtrl: NavController) {
-
   }
 
 }
